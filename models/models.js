e20b8812e9541cd1442a723584c9f3f235e51c06
sarabiae <sarabia.e@gmail.com>
2015-07-24 12:04:51 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -33,9 +33,10 @@ exports.Quiz = Quiz;
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
   //success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count){
-    if (count === 0) {
-      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'}).success(function() {
+  Quiz.count().then(function(count) {
+    if (count === 0) { //la tabla se inicializa solo si está vacía
+      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'});
+      Quiz.create({pregunta:'Capital de Portugal', respuesta:'Lisboa'}).then(function() {
         console.log('Base de datos inicializada');
       });
     };
