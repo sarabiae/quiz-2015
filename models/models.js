58f585ec1cb11a876954ff972d87ebc6117bd6d7
sarabiae <sarabia.e@gmail.com>
2015-07-24 10:46:23 +0200

    Base Datos Local Heroku

---
+++ models/models.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+//Bases de datos POSTGRES y SQLITE
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DB_name, user, pwd,{
+  dialect: protocol,
+  protocol: protocol,
+  port: port,
+  host: host,
+  storage: storage,
+  omitNull: true
+})
+
+//Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null, {dialect:"sqlite", storage: "quiz.sqlite"});
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+exports.Quiz = Quiz;
+
+//sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().success(function(){
+  //success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function(count){
+    if (count === 0) {
+      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'}).success(function() {
+        console.log('Base de datos inicializada');
+      });
+    };
+  });
+});
