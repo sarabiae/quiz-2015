42826f6092bbf7d47bdb1480a75889d2099f4725
sarabiae <sarabia.e@gmail.com>
2015-08-03 12:32:51 +0200

    Nuevo campo tema

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -41,7 +41,7 @@ exports.answer = function(req, res) {
 //GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( //crea objeto quiz
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
   );
   res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -50,20 +50,19 @@ exports.new = function(req, res) {
 exports.create = function(req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
 
-  quiz
-  .validate()
-  .then(
-    function(err) {
-      if (err) {
-        res.render('quizes/new', {quiz: quiz, errors: err.errors});
-      } else {
-        //guarda en la BD los campos pregunta y respuesta de quiz
-        quiz
-        .save({fields: ["pregunta", "respuesta"]})
-        .then(function(){res.redirect('/quizes')})
-      }
-    }
-  );
+  var errors = quiz.validate();
+  if (errors) {
+    var i = 0;
+    var errores = new Array();
+    for (var desc in errors)
+      errores[i++] = {message: errors[desc]};
+    res.render('quizes/new', {quiz: quiz, errors: errores});
+  } else {
+    //guarda en la BD los campos pregunta y respuesta de quiz
+    quiz
+    .save({fields: ["pregunta", "respuesta", "tema"]})
+    .then(function(){res.redirect('/quizes')})
+  }
 };
 
 //GET /quizes/:id/edit
@@ -76,21 +75,21 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
-  req.quiz
-  .validate()
-  .then(
-    function(err) {
-      if (err) {
-        res.render('quizes/edit', {quiz: req.quiz, error: err.errors});
-      } else {
-        req.quiz  //save: guarda campos pregunta y respuesta en DB
-        .save({fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes');});
-          //Redirección HTTP a la lista de preguntas (URL relativo)
-      }
-    }
-  );
+  var errors = req.quiz.validate();
+  if (errors) {
+    var i = 0;
+    var errores = new Array();
+    for (var desc in errors)
+      errores[i++] = {message: errors[desc]};
+    res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+  } else {
+    req.quiz  //save: guarda campos pregunta y respuesta en DB
+    .save({fields: ["pregunta", "respuesta", "tema"]})
+    .then( function(){ res.redirect('/quizes');});
+      //Redirección HTTP a la lista de preguntas (URL relativo)
+  }
 }
 
 //DELETE /quizes/:id
--- models/models.js
+++ models/models.js
@@ -35,8 +35,8 @@ sequelize.sync().success(function(){
   //success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function(count) {
     if (count === 0) { //la tabla se inicializa solo si está vacía
-      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'});
-      Quiz.create({pregunta:'Capital de Portugal', respuesta:'Lisboa'}).then(function() {
+      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma', tema:'ocio'});
+      Quiz.create({pregunta:'Capital de Portugal', respuesta:'Lisboa', tema:'ocio'}).then(function() {
         console.log('Base de datos inicializada');
       });
     };
