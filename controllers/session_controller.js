462a4e05ff8ede282019acf3dacce7e62389fd5c
sarabiae <sarabia.e@gmail.com>
2015-08-09 14:41:56 +0200

    Autenticación y sesión

---
+++ controllers/session_controller.js
@@ -0,0 +1,34 @@
+//GET /login -- Formulario Login
+exports.new = function(req, res) {
+  var errors = req.session.errors || {};
+  req.session.errors = {};
+
+  res.render('sessions/new', {errors: errors});
+};
+
+//POST /login -- Crear la sesion
+exports.create = function(req, res) {
+  var login = req.body.login;
+  var password = req.body.password;
+
+  var userController = require('./user_controller');
+  userController.autenticar(login, password, function(error, user) {
+    if (error) { //si hay error retornamos mensajes de error de sesción
+      req.session.errors = [{"message": 'Se ha producido un error: ' + error}];
+      res.redirect("/login");
+      return;
+    }
+
+    //Crear req.session.user y guardar campos id y username
+    //La session se define por la existencia de req.session.user
+    req.session.user = {id:user.id, username:user.username};
+
+    res.redirect(req.session.redir.toString()); //redirigir a path anterior a login
+  });
+};
+
+//DELETE /logout -- Destruir sesion
+exports.destroy = function(req, res) {
+  delete req.session.user;
+  res.redirect(req.session.redir.toString());
+};
